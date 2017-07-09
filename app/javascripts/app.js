c6d18fedb16f17f71fbebfb777465d77f568305a
Nikita Chebykin <nikita@chebyk.in>
2017-07-09 21:10:06 +0200

    Setup a basic config

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,3 +1,4 @@
+/* global web3 alert */
 // Import the page's CSS. Webpack will know what to do with it.
 import '../stylesheets/app.css'
 
@@ -5,33 +6,33 @@ import '../stylesheets/app.css'
 import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
 
-// Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import ProfileBuilderContract from '../../build/contracts/ProfileBuilder.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts)
+let Profile = contract(ProfileBuilderContract)
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-var accounts
-var account
+let accounts
+let account
 
 window.App = {
+  bootstrap: function () {
+    Profile.deployed().then((instance) => {
+      console.log(instance)
+    })
+  },
   start: function () {
-    var self = this
+    let self = this
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider)
+
+    Profile.setProvider(web3.currentProvider)
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
+      if (err !== null) {
         alert('There was an error fetching your accounts.')
         return
       }
 
-      if (accs.length == 0) {
+      if (accs.length === 0) {
         alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
         return
       }
@@ -39,49 +40,7 @@ window.App = {
       accounts = accs
       account = accounts[0]
 
-      self.refreshBalance()
-    })
-  },
-
-  setStatus: function (message) {
-    var status = document.getElementById('status')
-    status.innerHTML = message
-  },
-
-  refreshBalance: function () {
-    var self = this
-
-    var meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.getBalance.call(account, { from: account })
-    }).then(function (value) {
-      var balance_element = document.getElementById('balance')
-      balance_element.innerHTML = value.valueOf()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error getting balance; see log.')
-    })
-  },
-
-  sendCoin: function () {
-    var self = this
-
-    var amount = parseInt(document.getElementById('amount').value)
-    var receiver = document.getElementById('receiver').value
-
-    this.setStatus('Initiating transaction... (please wait)')
-
-    var meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.sendCoin(receiver, amount, { from: account })
-    }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      self.bootstrap()
     })
   }
 }
@@ -102,5 +61,5 @@ window.addEventListener('load', function () {
     window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
   }
 
-  App.start()
+  window.App.start()
 })
